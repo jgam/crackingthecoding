c55a0a037054a1f1585f5af0f0b88653f6c09c6e
jgam <jgam@alumni.nd.edu>
2019-02-27 15:54:15 +0900

    parittion

--- chpt2/questions.py
+++ chpt2/questions.py
@@ -30,10 +30,10 @@ class Test(unittest.TestCase):
 		self.assertEqual(head.next.data, 3)
 		self.assertEqual(head.next.next.data, 5)
 		self.assertEqual(head.next.next.next, None)
-
+"""
 if __name__ == "__main__":
 	unittest.main()
-
+"""
 #2.2
 # algorithm to find the kth to last element of a singly linked list
 def return_kth_to_last(head, k):
@@ -58,8 +58,90 @@ class Test(unittest.TestCase):
 		self.assertEqual(4, return_kth_to_last(head,4).data)
 		self.assertEqual(2, return_kth_to_last(head,6).data)
 		self.assertEqual(1, return_kth_to_last(head, 7))
-
+"""
 if __name__ == "__main__":
 	unittest.main()
+"""
+#2.3
+#delete the middle node
+
+class ListNode:
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+		return
+
+	def has_value(self, value):
+		if self.data == value:
+			return True
+		else:
+			return False
+
+class SingleLinkedList:
+	def __init__(self):
+		self.head = None
+		self.tail = None
+		return
+
+	def add_list_item(self, item):
+		if not isinstance(item, ListNode):
+			item = ListNode(item)
+
+		if self.head is None:
+			self.head = item
+		else:
+			self.tail.next = item
+
+		self.tail = item
+
+		return
+
+	def list_length(self):
+		count = 0
+		current_node = self.head
+
+		while current_node is not None:
+			count += 1
+			current_node = current_node.next
+
+		return count
+
+	def output_list(self):
+		current_node = self.head
+		while current_node is not None:
+			print(current_node.data)
+			current_node = current_node.next
+		return
+
+	def delete_middle_node(self):
+		current_node = self.head
+		list_range = self.list_length()//2
+		for _ in range(list_range):
+			current_node = current_node.next 
+		return current_node.data
+
+	#2.4 parittion
+	def partition(self, partition_value):
+		less_linked_list = SingleLinkedList()
+		large_linked_list = SingleLinkedList()
+		self_range = self.list_length()
+		for _ in range(self_range):
+			return 0
 
 
+#create four single nodes
+node1 = ListNode(15)
+node2 = ListNode(8.2)
+item3 = "Berlin"
+node4 = ListNode(15)
+node5 = ListNode(13)
+
+track = SingleLinkedList()
+print("track length: %i" % track.list_length()):
+
+for current_item in [node1, node2, item3, node4, node5]:
+	track.add_list_item(current_item)
+	print("track length: %i" % track.list_length())
+	track.output_list()
+print(track.output_list())
+print("deleted node", track.delete_middle_node())
